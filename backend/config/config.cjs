d8109de7a384bf2ce702f5463a4a494dfd6091f9
PlomoChavez <jesus.chavez@loyola.edu.mx>
2026-07-10 23:56:33 -0600

    conexion

--- backend/config/config.cjs
+++ backend/config/config.cjs
@@ -1,52 +1,22 @@
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  logging: process.env.NODE_ENV === "development",
+  define: {
+    paranoid: true,
+    timestamps: true,
+    underscored: true,
+    deletedAt: "deleted_at",
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  },
+};
+
 export default {
-  development: {
-    username: "volunta3_voluntariado",
-    password: "Jesus.34J",
-    database: "volunta3_vol2",
-    host: "mx58.hostgator.mx",
-    logging: true,
-    dialect: "mysql",
-    define: {
-      paranoid: true,
-      timestamps: true,
-      underscored: true,
-      deletedAt: "deleted_at",
-      createdAt: "created_at",
-      updatedAt: "updated_at",
-    },
-  },
-
-  test: {
-    username: "volunta3_voluntariado",
-    password: "Jesus.34J",
-    database: "volunta3_vol2",
-    host: "mx58.hostgator.mx",
-    logging: false,
-    dialect: "mysql",
-    define: {
-      paranoid: true,
-      timestamps: true,
-      underscored: true,
-      deletedAt: "deleted_at",
-      createdAt: "created_at",
-      updatedAt: "updated_at",
-    },
-  },
-
-  production: {
-    username: "volunta3_voluntariado",
-    password: "Jesus.34J",
-    database: "volunta3_vol2",
-    host: "mx58.hostgator.mx",
-    logging: false,
-    dialect: "mysql",
-    define: {
-      paranoid: true,
-      timestamps: true,
-      underscored: true,
-      deletedAt: "deleted_at",
-      createdAt: "created_at",
-      updatedAt: "updated_at",
-    },
-  },
+  development: config,
+  test: config,
+  production: config,
 };
